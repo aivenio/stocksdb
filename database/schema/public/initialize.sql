91b758b194d79e6759f552426089ffed3760d8e7
vigilantloki <dpramanik.pidilite@gmail.com>
2021-05-16 13:09:28 +0530

    ⚙ initialize public schema, move create statements under tables/

---
+++ database/schema/public/initialize.sql
@@ -0,0 +1,11 @@
+/********************************************************************
+Create Table(s) on the Public Schema for MacroDB
+********************************************************************/
+
+\i database/schema/public/tables/data_source.sql
+
+/********************************************************************
+Populate with Initial Seed Data
+********************************************************************/
+
+\i database/schema/public/seed/data_source.sql
