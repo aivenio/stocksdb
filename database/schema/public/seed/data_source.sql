cc2cedc7121a983d1a417af0ac02a84e02e53d68
vigilantloki <dpramanik.pidilite@gmail.com>
2021-05-16 12:46:32 +0530

    ✨ add data source table, initial seed data

---
+++ database/schema/public/seed/data_source.sql
@@ -0,0 +1,4 @@
+INSERT INTO private.data_source_mw (data_source_id, data_source_name, data_source_desc, data_source_uri) VALUES
+    ('ERAPI', 'Exchange Rates API', 'Historical and Real-Time Exchange Rates', 'https://exchangeratesapi.io/'),
+    ('XECCV', 'XE Currency Converter', 'Live Exchange Rates for Personal & Business', 'https://www.xe.com/currencyconverter/'),
+    ('OSEXR', 'Open Source Exchange Rates API', 'Currency API Data at a Granular Level', 'https://openexchangerates.org/');
