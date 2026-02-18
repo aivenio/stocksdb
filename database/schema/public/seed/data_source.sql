3d7771e3023637cb2487a483275c7efa4a6b84ed
vigilantloki <dpramanik.pidilite@gmail.com>
2026-02-18 20:37:18 +0530

    ✨ added data source for MIN ISO 10383 codes

--- database/schema/public/seed/data_source.sql
+++ database/schema/public/seed/data_source.sql
@@ -1,2 +1,3 @@
 INSERT INTO public.data_source_mw (data_source_id, data_source_name, data_source_desc, data_source_uri) VALUES
-    ('ISIN0', 'ISIN NSDL Data', 'ISIN List of NSDL Companies', 'https://raw.githubusercontent.com/captn3m0/india-isin-data/refs/heads/main/ISIN.csv');
+    ('ISIN0', 'ISIN NSDL Data', 'ISIN List of NSDL Companies', 'https://raw.githubusercontent.com/captn3m0/india-isin-data/refs/heads/main/ISIN.csv'),
+    ('MIC00', 'ISO 10383 MIC Codes', 'Global Market Identifier (MIC) Codes', 'https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.csv');
