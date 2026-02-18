153248b18a88fbcc7e0058e2a06c379cd6f6d08a
vigilantloki <dpramanik.pidilite@gmail.com>
2026-02-18 23:53:40 +0530

    🩹 patch tables to init time option registrations

--- database/schema/common/initialize.sql
+++ database/schema/common/initialize.sql
@@ -1,4 +1,4 @@
-\echo 'Creating Common Table(s) ...'
+\echo 'Creating Common Reference Table(s) ...'
 
 \i database/schema/common/reference/macrodb_currency.sql
 \i database/schema/common/reference/macrodb_geography.sql
@@ -6,3 +6,8 @@
 \echo 'Creating Common Data Type(s) ...'
 
 \i database/schema/common/types/security_issue_type.sql
+
+\echo 'Creating Common Table(s) ...'
+
+\i database/schema/common/tables/exchanges.sql
+\i database/schema/common/tables/securities.sql
