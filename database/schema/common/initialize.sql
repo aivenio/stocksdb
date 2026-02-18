8fb7b649e588ea879aeed5263a9e928b9bc6f4b4
vigilantloki <dpramanik.pidilite@gmail.com>
2026-02-18 20:27:53 +0530

    🩹⭐ move reference data tables into separate directory

--- database/schema/common/initialize.sql
+++ database/schema/common/initialize.sql
@@ -1,4 +1,4 @@
 \echo 'Creating Common Table(s) ...'
 
-\i database/schema/common/tables/macrodb_currency_reference.sql
-\i database/schema/common/tables/macrodb_geography_reference.sql
+\i database/schema/common/reference/macrodb_currency.sql
+\i database/schema/common/reference/macrodb_geography.sql
