f3fd08ef93f242bdce2b3c2a7ce7990877b8309d
vigilantloki <dpramanik.pidilite@gmail.com>
2026-02-20 20:02:04 +0530

    ✨ added NSE India as a data source list

--- database/schema/public/seed/data_source.sql
+++ database/schema/public/seed/data_source.sql
@@ -1,3 +1,5 @@
 INSERT INTO public.data_source_mw (data_source_id, data_source_name, data_source_desc, data_source_uri) VALUES
     ('ISIN0', 'ISIN NSDL Data', 'ISIN List of NSDL Companies', 'https://raw.githubusercontent.com/captn3m0/india-isin-data/refs/heads/main/ISIN.csv'),
-    ('MIC00', 'ISO 10383 MIC Codes', 'Global Market Identifier (MIC) Codes', 'https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.csv');
+    ('MIC00', 'ISO 10383 MIC Codes', 'Global Market Identifier (MIC) Codes', 'https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.csv'),
+    ('NSEMD', 'NSE Securities available for Trading', 'NSE List of Tradeable Securities', 'https://www.nseindia.com/static/market-data/securities-available-for-trading'),
+    ('NSEEQ', 'NSE MD Equity Segment', 'NSE List of Tradeable Equity Securities for Trading', 'https://nsearchives.nseindia.com/content/equities/EQUITY_L.csv');
