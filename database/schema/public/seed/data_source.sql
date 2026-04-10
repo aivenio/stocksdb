65c4a25ed3cfa0fd7a717e31bc89e447413db649
Debmalya Pramanik <debmalyapramanik.005@gmail.com>
2026-04-11 00:09:08 +0530

    ✨ added new data source - FYERS
    FYERS (https://myapi.fyers.in) is an Indian DEMAT Account that provides tools and services related to trading automations. The data source is added which can be used by the end users to create their own API Keys to fetch historical data and/or to execute orders.

--- database/schema/public/seed/data_source.sql
+++ database/schema/public/seed/data_source.sql
@@ -2,4 +2,5 @@ INSERT INTO public.data_source_mw (data_source_id, data_source_name, data_source
     ('ISIN0', 'ISIN NSDL Data', 'ISIN List of NSDL Companies', 'https://raw.githubusercontent.com/captn3m0/india-isin-data/refs/heads/main/ISIN.csv'),
     ('MIC00', 'ISO 10383 MIC Codes', 'Global Market Identifier (MIC) Codes', 'https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.csv'),
     ('NSEMD', 'NSE Securities available for Trading', 'NSE List of Tradeable Securities', 'https://www.nseindia.com/static/market-data/securities-available-for-trading'),
-    ('NSEEQ', 'NSE MD Equity Segment', 'NSE List of Tradeable Equity Securities for Trading', 'https://nsearchives.nseindia.com/content/equities/EQUITY_L.csv');
+    ('NSEEQ', 'NSE MD Equity Segment', 'NSE List of Tradeable Equity Securities for Trading', 'https://nsearchives.nseindia.com/content/equities/EQUITY_L.csv'),
+    ('FYERS', 'FYERS Historical Data API', 'API Service by Fyers DEMAT A/C', 'https://myapi.fyers.in');
